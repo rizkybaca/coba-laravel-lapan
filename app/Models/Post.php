ea63885d8fcac078831d9d80d62c2d92fa91ac51
ta <akun.rizky.baca@gmail.com>
2021-11-10 00:41:46 +0700

    post category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,13 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'title', 'excerpt', 'body'
-    ];
+    // protected $fillable = [
+    //     'title', 'excerpt', 'body'
+    // ];
+    protected $guarded = ['id'];
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,10 @@
 
 <article>
   <h2>{{ $post->title }}</h2>
-  <h5>By: {{ $post['author'] }}</h5>
-  {{-- <p>{{ $post->body }}</p> --}}
+
+
+  <h5>By: {{ $post['author'] }} in <a href="/categories/{{ $post->categories->slug }}">{{ $post->categories->name }}</a></h5>
+
   {!! $post->body !!}
 </article>
 <a href="/blog">Back to blog</a>
