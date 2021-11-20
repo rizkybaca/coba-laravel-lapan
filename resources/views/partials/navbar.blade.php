ca920ff8521cabe73f615ecf7e812ab0a53b8379
ta <akun.rizky.baca@gmail.com>
2021-11-20 17:58:23 +0700

    view login and register

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,16 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-      <div class="container">
-        <a class="navbar-brand" href="/">Yo, Coba!</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link {{ ($active==="home")?'active':'' }}" aria-current="page" href="/">Home</a>
-            <a class="nav-link {{ ($active==="about")?'active':'' }}" href="/about">About</a>
-            <a class="nav-link {{ ($active==="blog")?'active':'' }}" href="/blog">Blog</a>
-            <a class="nav-link {{ ($active==="categories")?'active':'' }}" href="/categories">Categories</a>
-          </div>
-        </div>
-      </div>
-    </nav>
+  <div class="container">
+    <a class="navbar-brand" href="/">Yo, coba!</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link {{ ($active==="home")?'active':'' }}" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active==="about")?'active':'' }}" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active==="blog")?'active':'' }}" href="/blog">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active==="categories")?'active':'' }}" href="/categories">Categories</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a href="/login" class="nav-link {{ ($active==="login")?'active':'' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
