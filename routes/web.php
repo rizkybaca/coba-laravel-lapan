c8fb8d2078d4a99aa6d66a61c6188feffc442afc
ta <akun.rizky.baca@gmail.com>
2021-11-20 11:49:19 +0700

    n+1 problem

--- routes/web.php
+++ routes/web.php
@@ -51,16 +51,15 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name,
+    return view('blog', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->posts->load('category', 'author'),
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('blog', [
-        'title' => 'User Posts',
-        'posts' => $author->posts
+        'title' => "Post by Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
